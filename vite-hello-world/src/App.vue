3e62c0a0fa823d3941d009a0ba92fe4b757bf802
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-23 14:55:29 +0100

    Ricreata tutta la configurazione da zero e creato un componente che stampa un titolo di pagina

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+  import PrintTitle from './components/PrintTitle.vue';
+
+  export default {
+    components: {
+      PrintTitle
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <PrintTitle/>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vite-hello-world/src/components/PrintTitle.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Il titolo della pagina'
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{title }}</h1>
+</template>
+
+<style scoped>
+  h1{
+    color: red;
+  }
+</style>
